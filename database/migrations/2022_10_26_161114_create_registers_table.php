ccd1d839053a28d67009fa5417bdf2b3d0ef6331
farhandardiri <faran.f4124n@gmail.com>
2022-10-26 23:19:12 +0700

    tambah Register Mdl Ctlr & table

---
+++ database/migrations/2022_10_26_161114_create_registers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registers', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->bigInteger('nik')->nullable();
+            $table->bigInteger('nohp1')->nullable();
+            $table->bigInteger('nohp2')->nullable();
+            $table->bigInteger('norekening')->nullable();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registers');
+    }
+};
